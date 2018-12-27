3e5f40462edeb8b506e0863520c6050fd9fa9479
gengteng <me@gteng.org>
2018-12-27 11:29:50 +0800

    add crawlo.rs

--- examples/crawlo.rs
+++ examples/crawlo.rs
@@ -1,20 +1,39 @@
-use spidero::Prey;
-use serde_json::to_string;
+// #![deny(warnings)]
 
+use std::mem;
+use std::io::{self, Cursor};
+use futures::{Future, Stream};
+use reqwest::r#async::{Client, Decoder};
+use tokio::fs::OpenOptions;
+
+fn crawl(keyword: &str) -> impl Future<Item=(), Error=()> {
+    Client::new()
+        .get(&format!("http://www.baidu.com/s?wd={}", keyword))
+        .send()
+        .and_then(|mut res| {
+            println!("{}", res.status());
+
+            let body = mem::replace(res.body_mut(), Decoder::empty());
+            body.concat2()
+        })
+        .map_err(|err| println!("request error: {}", err))
+        .map(Cursor::new)
+        .and_then(move|mut body| {
+            OpenOptions::new().create(true).write(true).open("./prey.html").and_then(move |mut f| {
+                let _ = io::copy(&mut body, &mut f)
+                    .map_err(|err| {
+                        println!("stdout error: {}", err);
+                    });
+                Ok(())
+            }).map_err(|_|{})
+        })
+}
 
 fn main() {
-    let prey = Prey {
-        url: "https://google.com/",
-        title: "hahaha",
-        content: "wtf"
-    };
-
-    match to_string(&prey) {
-        Ok(s) =>
-            match serde_json::from_str::<Prey>(&s) {
-                Ok(p) => println!("{:?}", p),
-                Err(e) => eprintln!("deserialize err: {}", e)
-            },
-        Err(e) => eprintln!("serialize err: {}", e)
+    if let Some(keyword) = std::env::args().nth(1) {
+        tokio::run(crawl(&keyword));
+    } else {
+        println!("no keyword provided");
     }
+
 }
